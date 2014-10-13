3c15c63496e6a03b13f586c15754e1d0e92106b6
Rachelle <rachellesalvadora@gmail.com>
2014-10-13 15:03:53 -0700

    index.php proper div php

--- wp-content/themes/first-theme/index.php
+++ wp-content/themes/first-theme/index.php
@@ -8,6 +8,10 @@
 
 <div class="main-content col-md-8" role="main">
 	<?php if ( have_posts() ): while( have_posts() ) : the_post(); ?>
-		hello <!--- print hello if have post -->
+		<?php get_template_part( 'content', get_post_format() ); ?>
 	<?php endwhile; ?>
+
+	<?php else : ?>
+		<php get_template_part( 'content', 'none' ); ?>
 	<?php endif; ?>
+</div> <!-- end main-content -->
