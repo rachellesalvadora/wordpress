7c43b93f5c66d50e8ad3c987b3394414207b81f8
Rachelle <rachellesalvadora@gmail.com>
2014-10-13 00:15:59 -0700

    Theme default and register various supported features

--- wp-content/themes/first-theme/functions.php
+++ wp-content/themes/first-theme/functions.php
@@ -35,4 +35,56 @@ if ( ! isset( $content_width ) ) {
 	$content_width = 800;
 }
 
+/**
+ * ---------------------------------------------------------------------------------------------
+ * 4.0 - Set up the theme default and register various supported features.
+ * ---------------------------------------------------------------------------------------------
+ */
+
+if ( ! function_exists( 'manak_setup' ) ) {
+	function manak_setup() {
+		/**
+		 * Make the theme available for translation.
+		 */
+		$lang_dir = THEMEROOT . '/languages';
+		load_theme_textdomain( 'alpha', $lang_dir );
+
+		/**
+		 * Add support for post formats.
+		 */
+		add_theme_supprt( 'post-formats', 
+			array(
+				'gallery',
+				'link',
+				'image',
+				'qoute',
+				'video',
+				'audio'
+			)
+		);
+
+		/**
+		 * Add support for automatic feed links.
+		 */
+		add_theme_support( 'automatic-feed-links' );
+
+		/**
+		 * Add support for post thumbnails.
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/**
+		 * Register nav menus.
+		 */
+		register_nav_menus(
+			array(
+				'main_menu' => --( 'Main Menu', 'alpha' )
+			)
+		);
+
+	}
+
+	add_action( 'after_theme_setup', 'manak_setup' );
+}
+
 ?>
