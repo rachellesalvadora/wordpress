79d7a29ac2e8065fadf6f236044165c18cc4a53e
Rachelle <rachellesalvadora@gmail.com>
2014-10-13 15:35:44 -0700

    content.php post title link

--- wp-content/themes/first-theme/content.php
+++ wp-content/themes/first-theme/content.php
@@ -9,5 +9,20 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	Article <!--Print if there is an article -->
+		<!-- Article header -->
+	<header class="entry-header"> <?php
+		// If the post has a thumbnail and it's not password protected
+		//then display the thumbnail
+		if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+			<figure class="entry-thumbnail"><?php the_post_thumbnail(); ?> </figure>
+		<?php endif; 
+
+		// If single page, display the title
+		// Else, we display the title in a link
+		if ( is_single() ) : ?>
+			<h1><?php the_title(); ?></h1>
+		<?php else : ?>
+			<h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+		<?php endif; ?>
+	</header>
 </article>
