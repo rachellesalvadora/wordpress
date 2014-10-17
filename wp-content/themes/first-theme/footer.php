59fcb286f91bace5a00b8a578fbe5991a8467d46
Rachelle <rachellesalvadora@gmail.com>
2014-10-16 19:26:57 -0700

    footer.php defined

--- wp-content/themes/first-theme/footer.php
+++ wp-content/themes/first-theme/footer.php
@@ -12,3 +12,11 @@
 			
 		</div> <!-- end row -->
 	</div> <!-- end container -->
+
+	<!-- FOOTER -->
+	<footer class="site-footer">
+		<div class="container">
+			<?php get_sidebar( 'footer' ); ?>
+			
+		</div> <!-- end container -->
+	</footer> <!-- end site-footer -->
